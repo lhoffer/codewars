150f88fe46843c719f52dbdd4249c0b6bbd5ed68
Luis Hoffer <lhoffer31@gmail.com>
2022-06-21 23:15:18 -0700

    codewars 8ku star trek challege

--- js/solution21_8ku_final_grade.js
+++ js/solution21_8ku_final_grade.js
@@ -12,3 +12,12 @@ function finalGrade (exam, projects) {
 }
 
 finalGrade(75, 2)
+
+// shorter example solution from codewars
+
+function finalGrade (exam, projects) {
+  if(exam > 90 || projects > 10) return 100;
+  if(exam > 75 & projects >= 5) return 90;
+  if(exam > 50 & projects >= 2) return 75;
+  return 0;
+}
