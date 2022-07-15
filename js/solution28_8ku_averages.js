6a4b228a22e0aa56b942a68d64fd7e248456bf38
Luis Hoffer <lhoffer31@gmail.com>
2022-07-14 23:36:02 -0700

    codewars 8ku reverse array

--- js/solution28_8ku_averages.js
+++ js/solution28_8ku_averages.js
@@ -11,3 +11,31 @@ function find_average(array) {
   }  
 }
 find_average([1,5,6])
+
+function index(array, n) {
+  if (array.length < n) {
+          return -1
+      }  else if (array.length >= n ) { 
+          for (let i = 0; i < array.length; i++) {
+              if (array.indexOf(i) == n) {
+              return i**n
+              } 
+          }
+      }
+  }   
+  index([4,2],2)
+
+  function index(array, n) {
+  console.log(array.length)
+  console.log(n)
+  }
+  index([4,2],2)
+
+  function index(array, n) {
+   if (n > array.length) {
+        return -1
+    }
+  }
+  index([4,2],2)
+
+ 
---
+++ js/solution32_8ku_reverse_array.js
@@ -0,0 +1,7 @@
+function reverseList(list) {
+  // reverse array
+  list.reverse()
+  // return array
+  return list
+}
+reverseList([1, 2, 3, 4])
